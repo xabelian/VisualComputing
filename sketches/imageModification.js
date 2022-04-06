3e28ba7ad12f78f23f66770e301ae1ec648ddd4f
xabelian <xabelian@users.noreply.github.com>
2022-04-06 13:21:37 +0000

    deploy: 60585f5000486870c10f192cad6725e22bd9d598

--- sketches/imageModification.js
+++ sketches/imageModification.js
@@ -1,27 +1,28 @@
 let img;
-function preload() {
-  img = loadImage('/VisualComputing/sketches/original.jpg');
-}
+    function preload() {
+    img = loadImage('https://xabelian.github.io/VisualComputing/sketches/original.jpg');
+    }
 
-function setup() {
-  createCanvas(404, 402);
-  image(img, 0, 0, width, height);
-  let d = pixelDensity();
-  loadPixels();
-        for (var y = 0; y < height*4; y++) {
-            for (var x = 0; x < width; x++) {
-              var index = (x + y * width)*4;
-              var r = pixels[index+0];
-              var g = pixels[index+1];
-              var b = pixels[index+2];
-              var a = pixels[index+3];     
-              
-              if (g > 80){
-                pixels[index+0] = r+70
-              }
+    function setup() {
+    createCanvas(404, 402);
+    image(img, 0, 0, width, height);
+    let d = pixelDensity();
+    loadPixels();
+            for (var y = 0; y < height*4; y++) {
+                for (var x = 0; x < width; x++) {
+                var index = (x + y * width)*4;
+                var r = pixels[index+0];
+                var g = pixels[index+1];
+                var b = pixels[index+2];
+                var a = pixels[index+3];     
+                
+                if (g > 80){
+                    pixels[index+2] = b+70
+                    //pixels[index+1] = g-40
+                }
 
-              
+                
+            }
         }
-      }
-  updatePixels();
+    updatePixels();
 }
